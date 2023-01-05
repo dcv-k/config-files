b330a0f5e1b1c04d76baed4e35bc15aa3e21a829
chandrabhanu <dcv.kalansooriya@gmail.com>
2023-01-05 10:08:55 +0530

    github token

---
+++ github.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+echo "git token for bhanu-000.github.io expires in Jan 18 2023"
+git_token_bhanu=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
+echo $git_token_bhanu
+echo "*******"
+echo "git token for all repos expires in Mar 23 2023"
+git_token_all=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
+echo $git_token_all
+
